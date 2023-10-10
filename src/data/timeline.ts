80d5d140fa131dd0bbf8f630d9fbe2257e841b90
Made Baruna <made.setia@gmail.com>
2023-10-11 01:32:11 +0800

    Update timeline

--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -4,65 +4,6 @@ const timeline: Array<Timeline[]> = [
 	[
 		{
 			name: 'Planar Fissure',
-			start: '2023-08-02 04:00:00',
-			end: '2023-08-09 03:59:00',
-			image: 'Planar Fissure.png',
-			color: '#6a77f4',
-			pos: '100%, -50%',
-			zoom: 1.5,
-			url: 'https://www.hoyolab.com/article/20047496',
-			show: true
-		}
-	],
-	[
-		{
-			name: 'Where Are You, Mystery Trotter?',
-			start: '2023-07-28 12:00:00',
-			end: '2023-08-14 03:59:00',
-			image: 'Where Are You, Mystery Trotter.png',
-			color: '#f1e7db',
-			pos: '30%, -20%',
-			zoom: 1.5,
-			url: 'https://www.hoyolab.com/article/20432934',
-			show: true
-		},
-		{
-			name: 'Realm of the Strange',
-			start: '2023-08-19 04:00:00',
-			end: '2023-08-26 03:59:00',
-			image: 'Realm of the Strange.png',
-			color: '#70d2fd',
-			pos: '30%, -30%',
-			zoom: 1.5,
-			url: 'https://www.hoyolab.com/article/20641646',
-			show: true
-		}
-	],
-	[
-		{
-			name: 'Tales of the Fantastic',
-			start: '2023-07-21 12:00:00',
-			end: '2023-08-07 03:59:00',
-			image: 'Tales of the Fantastic.png',
-			color: '#df9b61',
-			pos: '0%, -50%',
-			zoom: 1,
-			url: 'https://www.hoyolab.com/article/20047496',
-			show: true
-		},
-		{
-			name: 'Underground Treasure Hunt',
-			start: '2023-08-09 12:00:00',
-			end: '2023-08-28 03:59:00',
-			image: 'Underground Treasure Hunt.png',
-			color: '#f0d397',
-			pos: '20%, -15%',
-			zoom: 1.7,
-			url: 'https://www.hoyolab.com/article/20711907',
-			show: true
-		},
-		{
-			name: 'Planar Fissure',
 			start: '2023-09-15 04:00:00',
 			end: '2023-09-22 03:59:00',
 			image: 'Planar Fissure.png',
@@ -82,21 +23,20 @@ const timeline: Array<Timeline[]> = [
 			zoom: 1,
 			url: 'https://www.hoyolab.com/article/21613902',
 			show: true
+		},
+		{
+			name: 'Planar Fissure',
+			start: '2023-10-20 04:00:00',
+			end: '2023-10-27 03:59:00',
+			image: 'Planar Fissure.png',
+			color: '#6a77f4',
+			pos: '30%, -40%',
+			zoom: 1.2,
+			url: 'https://www.hoyolab.com/article/22183093',
+			show: true
 		}
 	],
 	[
-		{
-			name: 'Gift of Odyssey	- Daily Login',
-			start: '2023-07-19 06:00:00',
-			end: '2023-08-29 03:59:00',
-			image: 'Gift of Odyssey.png',
-			color: '#fff7db',
-			pos: '0%, -32%',
-			zoom: 1,
-			url: 'https://www.hoyolab.com/article/20047496',
-			timezoneDependent: true,
-			show: true
-		},
 		{
 			name: 'Aurum Alley',
 			start: '2023-08-31 12:00:00',
@@ -118,32 +58,21 @@ const timeline: Array<Timeline[]> = [
 			zoom: 1.2,
 			url: 'https://www.hoyolab.com/article/21613902',
 			show: true
-		}
-	],
-	[
+		},
 		{
-			name: 'A Lost Soul - Blade Warp Event',
-			start: '2023-07-19 06:00:00',
-			end: '2023-08-09 11:59:00',
-			image: 'A Lost Soul 2007.png',
-			color: '#7d90c3',
-			pos: '0%, 17%',
-			zoom: 2,
-			url: 'https://www.hoyolab.com/article/20114086',
+			name: 'Aetherium Wars',
+			start: '2023-10-11 06:00:00',
+			end: '2023-11-13 03:59:00',
+			image: 'Aetherium Wars.png',
+			color: '#a2c2fd',
+			pos: '30%, -10%',
+			zoom: 1.2,
+			url: 'https://www.hoyolab.com/article/22227545',
 			timezoneDependent: true,
 			show: true
 		},
-		{
-			name: 'Nessun Dorma - Kafka Warp Event',
-			start: '2023-08-09 12:00:00',
-			end: '2023-08-29 14:59:00',
-			image: 'Nessun Dorma 2008.png',
-			color: '#cba2df',
-			pos: '0%, 12%',
-			zoom: 2,
-			url: 'https://www.hoyolab.com/article/20713096',
-			show: true
-		},
+	],
+	[
 		{
 			name: 'Epochal Spectrum - Dan Heng • Imbibitor Lunae Warp Event',
 			start: '2023-08-30 06:00:00',
@@ -166,32 +95,21 @@ const timeline: Array<Timeline[]> = [
 			zoom: 2,
 			url: 'https://www.hoyolab.com/article/21723945',
 			show: true
+		},
+		{
+			name: 'Gentle Eclipse of the Moon - Jingliu Warp Event',
+			start: '2023-10-11 06:00:00',
+			end: '2023-10-27 11:59:00',
+			image: 'Gentle Eclipse of the Moon 2011.png',
+			color: '#51aaff',
+			pos: '5%, 13%',
+			zoom: 2,
+			url: 'https://www.hoyolab.com/article/22226154',
+			timezoneDependent: true,
+			show: true
 		}
 	],
 	[
-		{
-			name: 'Brilliant Fixation - Light Cone Warp Event',
-			start: '2023-07-19 06:00:00',
-			end: '2023-08-09 11:59:00',
-			image: 'Brilliant Fixation 3007.png',
-			color: '#8c9ba5',
-			pos: '-5%, 5%',
-			zoom: 2,
-			url: 'https://www.hoyolab.com/article/20114188',
-			timezoneDependent: true,
-			show: true
-		},
-		{
-			name: 'Brilliant Fixation - Light Cone Warp Event',
-			start: '2023-08-09 12:00:00',
-			end: '2023-08-29 14:59:00',
-			image: 'Brilliant Fixation 3008.png',
-			color: '#cc7f95',
-			pos: '-5%, 0%',
-			zoom: 2,
-			url: 'https://www.hoyolab.com/article/20713150',
-			show: true
-		},
 		{
 			name: 'Brilliant Fixation - Light Cone Warp Event',
 			start: '2023-08-30 06:00:00',
@@ -215,21 +133,21 @@ const timeline: Array<Timeline[]> = [
 			url: 'https://www.hoyolab.com/article/21244077',
 			timezoneDependent: true,
 			show: true
+		},
+		{
+			name: 'Brilliant Fixation - Light Cone Warp Event',
+			start: '2023-10-11 06:00:00',
+			end: '2023-10-27 11:59:00',
+			image: 'Brilliant Fixation 3011.png',
+			color: '#c6d2ff',
+			pos: '0%, 0%',
+			zoom: 2,
+			url: 'https://www.hoyolab.com/article/22226154',
+			timezoneDependent: true,
+			show: true
 		}
 	],
 	[
-		{
-			name: 'Nameless Honor - Battle Pass',
-			start: '2023-07-19 06:00:00',
-			end: '2023-08-28 03:59:00',
-			image: 'Nameless Honor.png',
-			color: '#ff9d92',
-			pos: '30%, -5%',
-			zoom: 2,
-			url: 'https://www.hoyolab.com/article/20049272',
-			timezoneDependent: true,
-			show: true
-		},
 		{
 			name: 'Nameless Honor - Battle Pass',
 			start: '2023-08-30 06:00:00',
@@ -239,6 +157,18 @@ const timeline: Array<Timeline[]> = [
 			pos: '30%, -5%',
 			zoom: 2,
 			url: 'https://www.hoyolab.com/article/21197343',
+			timezoneDependent: true,
+			show: true
+		},
+		{
+			name: 'Nameless Honor - Battle Pass',
+			start: '2023-10-11 06:00:00',
+			end: '2023-11-13 03:59:00',
+			image: 'Nameless Honor.png',
+			color: '#ff9d92',
+			pos: '30%, -5%',
+			zoom: 2,
+			url: 'https://www.hoyolab.com/article/21995336',
 			timezoneDependent: true,
 			show: true
 		}
